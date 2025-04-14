245771d11d0c8c193787a210cd3e9ce5c82abd06
Jonathan Bowe <jbowe@webstaurantstore.com>
2025-04-14 08:34:19 -0400

    Update to Support UptimeRobot V2 API
    Update uptimerobot-go SDK to support the v2 API, specifically:
    - `api/api.go`
        - "Parameters" like api key are expected to be provided via the body.
        - Add default headers (Cache-Control and Content-Type) to allow passing url encoded string as body.
        - Update requestURI to include `/v2`.
    - `api/accountdetails_test.go`
        - Fix typo to use Fatalf rather than Fatal.
    - `api/accountdetails.go`
        - Add additional fields that are now included in /getAccountDetails response.
        - Update to POST rather than GET.
    - `api/monitors_test.go`
        - Fix typo to use Fatalf/Logf rather than Fatal/Log.
        - Fix typo in capitalization for monitorId.
    - `api/monitors.go`
        - Per [/editMonitor documentation](https://uptimerobot.com/api/#editMonitorWrap), type of monitor cannot be edited. Removed MonitorType from EditMonitorRequest struct.
        - Updated parameter names and XML struct tags to match api v2.
        - Ensured XML unmarshaling works for response time lists.
    
    As a part of this change I ensured that all tests passed. This adds little to no functionality, simply ensures that the current functionality works with the currently available API.
    
    Additionally, I have added a go.mod so this can be properly built and imported as a package

--- api/monitors.go
+++ api/monitors.go
@@ -24,7 +24,6 @@ type EditMonitorRequest struct {
 	Id           int
 	FriendlyName string
 	Url          string
-	MonitorType  MonitorType
 }
 
 type DeleteMonitorRequest struct {
@@ -44,13 +43,18 @@ type XMLMonitors struct {
 }
 
 type XMLMonitor struct {
-	ID            int               `xml:"id,int,attr"`
-	FriendlyName  string            `xml:"friendlyname,string,attr"`
-	URL           string            `xml:"url,string,attr"`
-	ResponseTimes []XMLResponseTime `xml:"responsetime"`
+	ID               int                 `xml:"id,int,attr"`
+	FriendlyName     string              `xml:"friendly_name,string,attr"`
+	URL              string              `xml:"url,string,attr"`
+	ResponseTimeList XMLResponseTimeList `xml:"response_times"`
 }
 
-type XMLResponseTime struct {
+type XMLResponseTimeList struct {
+	ResponseTimes []ResponseTime `xml:"response_time"`
+}
+
+type ResponseTime struct {
+	Date  int `xml:"datetime,int,attr"`
 	Value int `xml:"value,int,attr"`
 }
 
@@ -65,7 +69,7 @@ func (c *Client) Monitors() *Monitors {
 }
 
 func (ad *Monitors) New(req NewMonitorRequest) (*MonitorResponse, error) {
-	r := ad.c.newRequest("GET", "/newMonitor")
+	r := ad.c.newRequest("POST", "/newMonitor")
 	err := r.setNewMonitorRequest(req)
 	if err != nil {
 		return nil, err
@@ -91,14 +95,16 @@ func (r *request) setNewMonitorRequest(req NewMonitorRequest) error {
 	if req.Url == "" {
 		return errors.New("Url: required value")
 	}
-	r.params.Set("MonitorFriendlyName", req.FriendlyName)
-	r.params.Set("MonitorURL", req.Url)
-	r.params.Set("MonitorType", strconv.Itoa(int(req.MonitorType)))
+	r.body.Set("friendly_name", req.FriendlyName)
+	r.body.Set("url", req.Url)
+	r.body.Set("type", strconv.Itoa(int(req.MonitorType)))
 	return nil
 }
 
+// Per https://uptimerobot.com/api/#editMonitorWrap, monitor type cannot be edited.
+// To change type, monitors should be deleted and recreated.
 func (ad *Monitors) Edit(req EditMonitorRequest) (*MonitorResponse, error) {
-	r := ad.c.newRequest("GET", "/editMonitor")
+	r := ad.c.newRequest("POST", "/editMonitor")
 	err := r.setEditMonitorRequest(req)
 	if err != nil {
 		return nil, err
@@ -121,21 +127,18 @@ func (r *request) setEditMonitorRequest(req EditMonitorRequest) error {
 	if req.Id == 0 {
 		return errors.New("Id: required value")
 	}
-	r.params.Set("monitorID", strconv.Itoa(req.Id))
+	r.body.Set("id", strconv.Itoa(req.Id))
 	if req.FriendlyName != "" {
-		r.params.Set("monitorFriendlyName", req.FriendlyName)
+		r.body.Set("friendly_name", req.FriendlyName)
 	}
 	if req.Url != "" {
-		r.params.Set("monitorURL", req.Url)
-	}
-	if int(req.MonitorType) != 0 {
-		r.params.Set("MonitorType", strconv.Itoa(int(req.MonitorType)))
+		r.body.Set("url", req.Url)
 	}
 	return nil
 }
 
 func (ad *Monitors) Delete(req DeleteMonitorRequest) (*MonitorResponse, error) {
-	r := ad.c.newRequest("GET", "/deleteMonitor")
+	r := ad.c.newRequest("POST", "/deleteMonitor")
 	err := r.setDeleteMonitorRequest(req)
 	if err != nil {
 		return nil, err
@@ -158,12 +161,12 @@ func (r *request) setDeleteMonitorRequest(req DeleteMonitorRequest) error {
 	if req.Id == 0 {
 		return errors.New("Id: required value")
 	}
-	r.params.Set("monitorID", strconv.Itoa(req.Id))
+	r.body.Set("id", strconv.Itoa(req.Id))
 	return nil
 }
 
 func (ad *Monitors) Get(req GetMonitorsRequest) (*XMLMonitors, error) {
-	r := ad.c.newRequest("GET", "/getMonitors")
+	r := ad.c.newRequest("POST", "/getMonitors")
 	err := r.setGetMonitorsRequest(req)
 	if err != nil {
 		return nil, err
@@ -185,10 +188,10 @@ func (ad *Monitors) Get(req GetMonitorsRequest) (*XMLMonitors, error) {
 
 func (r *request) setGetMonitorsRequest(req GetMonitorsRequest) error {
 	if req.MonitorId == 0 {
-		return errors.New("monitors: required value")
+		return errors.New("MonitorId: required value")
 	}
-	r.params.Set("monitors", strconv.Itoa(req.MonitorId))
-	r.params.Set("responseTimes", "1")
-	r.params.Set("responseTimesAverage", "300")
+	r.body.Set("monitors", strconv.Itoa(req.MonitorId))
+	r.body.Set("response_times", "1")
+	r.body.Set("response_times_average", "300")
 	return nil
 }
